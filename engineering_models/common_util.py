3538417d0881d33804f493dc993cbc158a5910e6
ml-ME-gatech <61564229+ml-ME-gatech@users.noreply.github.com>
2024-11-13 20:27:32 -0500

    Paramteric Application
    Worked on parametric application notebook. Updated modeling of conductivity, uniform elongation, UTS. to include additional analysis of "influential" and "leverage" points.  This sort-of changed the models but not all that much (to be expected).

--- engineering_models/common_util.py
+++ engineering_models/common_util.py
@@ -1,11 +1,13 @@
 import numpy as np
 from matplotlib import pyplot as plt
-from scipy.stats import norm
-from typing import List, Tuple, Callable    
+from typing import List, Tuple, Callable, Any    
 from sklearn.preprocessing import MinMaxScaler  
-from sklearn.linear_model import LassoLarsIC, LassoLarsCV
+from sklearn.linear_model import LassoLarsIC, LassoLarsCV, LinearRegression
 import pandas as pd
 from matplotlib.ticker import AutoMinorLocator
+from functools import partial
+from sklearn.pipeline import Pipeline
+from numpy.polynomial import Polynomial
 
 """
 Common utilities used among the jupyter notebooks in this folder
@@ -105,6 +107,10 @@ class SklearnTransform:
         self.fit(x)
         return self.transform(x)  
     
+    def __len__(self):
+        return len(self.transforms)
+    
+    
 class OneDimensionalBasisExpansion:
     """ 
     expand the basis of the regressor variables to include
@@ -114,6 +120,9 @@ class OneDimensionalBasisExpansion:
 
         self.features = features
 
+    def __len__(self):
+        return len(self.features)
+    
     def transform(self,x: np.ndarray) -> np.ndarray:
         return np.concatenate([tform(x) for tform in self.features],axis = 1)
     
@@ -170,11 +179,33 @@ def setup_axis_default(ax: plt.Axes):
     """
     convinience function to set up the axis
     """
-    ax.set_xticks(np.arange(0,1500,300))
-    ax.set_yticks(np.arange(200,1900,300))
+    ax.tick_params('both',labelsize = 11,which = 'both',direction = 'in')
     ax.xaxis.set_minor_locator(AutoMinorLocator(5))
     ax.yaxis.set_minor_locator(AutoMinorLocator(5))
     return ax
+
+def identity(x: np.ndarray) -> np.ndarray:
+    return x
+
+def inverted(x: np.ndarray) -> np.ndarray:
+    return 1/x
+
+def xlogx(x: np.ndarray) -> np.ndarray:
+    return x*np.log(x)
+
+def power(power: float,x: np.ndarray) -> np.ndarray:
+    return np.power(x,power)
+
+def function_product(func1: Callable,
+                     func2: Callable,
+                     x: np.ndarray) -> np.ndarray:
+    return func1(x)*func2(x)
+
+def constant(const: float,x: np.ndarray) -> np.ndarray:
+    return np.ones_like(x)*const
+
+def power_derivative(p: float) -> np.ndarray:
+    return partial(function_product,partial(power,p-1),partial(constant,p))
 
 def get_k_most_commmon_feature_transform(data:List[Tuple[np.ndarray,np.ndarray]],
                                          k:int,
@@ -189,13 +220,13 @@ def get_k_most_commmon_feature_transform(data:List[Tuple[np.ndarray,np.ndarray]]
     table = dict()
 
 
-    input_features =  [TransformedFeature('x',lambda x: x),
-                TransformedFeature('log x',lambda x: np.log(x)),
-                TransformedFeature('x^0.5',lambda x: x**0.5),
-                TransformedFeature('1/x',lambda x: 1/x),
-                TransformedFeature('x log x',lambda x: x*np.log(x)),
-                TransformedFeature('x^2',lambda x: x**2),
-                TransformedFeature('x^2 log x',lambda x: x**2*np.log(x))] if input_features is None else input_features
+    input_features =  [TransformedFeature('x',identity),
+                TransformedFeature('log x',np.log),
+                TransformedFeature('x^0.5',np.sqrt),
+                TransformedFeature('1/x',inverted),    
+                TransformedFeature('x log x',xlogx),
+                TransformedFeature('x^2',partial(power,2)),
+                TransformedFeature('x^2 log x',partial(function_product,partial(power,2),np.log))] if input_features is None else input_features
     
     data_len = 0
     for (x,y) in data:
@@ -224,6 +255,138 @@ def get_k_most_commmon_feature_transform(data:List[Tuple[np.ndarray,np.ndarray]]
         features.append(table[key])
     
     transform = OneDimensionalBasisExpansion(
-        [TransformedFeature('1',lambda x: np.ones_like(x),derivative= lambda x: np.zeros_like(x))] + features)
+        [TransformedFeature('1',partial(constant,1.),derivative= partial(constant,0.))] + features)
 
     return transform
+
+class SklearnLMDeriv:
+
+    def __init__(self,pipe: Pipeline) -> None:
+        self.pipe = pipe
+
+    def __call__(self,Xnew: np.ndarray) -> np.ndarray:
+        tform = self.pipe.named_steps['transform'].deriv(Xnew)
+        return tform @ self.pipe.named_steps['model'].coef_
+
+
+class StichPolynomialSplineModel:
+
+    def __init__(self,pipe: Pipeline) -> None:
+        
+        self.model = pipe
+        self.model_deriv = SklearnLMDeriv(pipe)
+        self.poly = None
+        self.break_points = None
+
+    def fit_spline(self, break_points: np.ndarray):
+        
+        self.break_points = break_points    
+        A= np.zeros((4,4))
+        A[0:2,:] = np.concatenate([break_points**r for r in range(4)],axis = 1)
+        A[2:,:] = np.concatenate([np.zeros_like(break_points)] + [r*break_points**(r-1) for r in range(1,4)],axis = 1)
+        b = np.zeros(4)
+        b[2] = self.model_deriv(break_points[0:1,:])
+        b[3] = self.hl_model_deriv(break_points[1:,:])
+        b[0] = self.model.predict(break_points[0:1,:])
+        b[1] = self.hl_model(break_points[1:,:])
+        self.poly = Polynomial(np.linalg.solve(A,b).squeeze())
+    
+    def fit(self, x: np.ndarray,y: np.ndarray, break_points: np.ndarray) -> None:
+        self.model.fit(x,y)
+        self.model_deriv = SklearnLMDeriv(self.model)
+        self.fit_spline(break_points)
+
+    def predict(self,x: np.ndarray) -> np.ndarray:
+        y = np.zeros_like(x)
+        
+        mask = x[:,0] <= self.break_points[0]
+        if mask.any() and x.shape[0] > 0:
+            y[mask,...] = self.model.predict(x[mask,...])[:,np.newaxis]
+        
+        mask = np.all([x > self.break_points[0],x < self.break_points[1]],axis = 0).squeeze()
+        if mask.any() and x.shape[0] > 0:
+            y[mask,...] = self.poly(x[mask,...])
+        
+        mask = np.squeeze(x >= self.break_points[1])
+        if mask.any() and x.shape[0] > 0:
+            y[mask,...] = self.hl_model(x[mask,...])
+        
+        return y
+
+    @staticmethod
+    def hl_model(x: np.ndarray) -> np.ndarray:
+        poly_hl = Polynomial([137.976060119897,-0.03859083192697,1.19769362e-5,-1.484e-9])
+        return poly_hl(x) + 3.866e6/(x + 273.15)**2
+
+    @staticmethod
+    def hl_model_deriv(x: np.ndarray) -> np.ndarray:
+        poly_hl = Polynomial([137.976060119897,-0.03859083192697,1.19769362e-5,-1.484e-9])
+        return poly_hl.deriv()(x) - 2*3.866e6/(x + 273.15)**3
+    
+def hdi(samples_: np.ndarray,alpha: int) -> np.ndarray:
+    """
+    Compute the highest density interval at level alpha
+    based upon samples from the distribution provided by "samples"
+    along the last axis of the array
+
+    Parameters
+    ----------
+    samples : np.ndarray
+        Samples from the distribution of interest
+    alpha : int
+
+    Returns
+    -------
+    np.ndarray
+        The HDI at level alpha
+
+    """
+
+    samples = samples_.reshape([-1,samples_.shape[-1]])
+    samples = np.sort(samples,axis = -1)
+    n = samples.shape[-1] 
+    n_included = int(np.floor(alpha*n))
+    n_intervals = n - n_included
+    interval_width = samples[:,n_included:] - samples[:,:n_intervals]
+    min_idx = np.argmin(interval_width,axis = -1)
+    hdi_min = samples[np.arange(samples.shape[0],dtype = int),min_idx]
+    hdi_max = samples[np.arange(samples.shape[0],dtype = int),min_idx+n_included]   
+
+    return np.stack([hdi_min,hdi_max],axis = -1)
+
+def get_p_from_generic_model(model: LinearRegression | Pipeline | Callable) -> int:
+    """
+    try and get the number of parameters from a generic model
+    """
+
+    if isinstance(model,Pipeline):
+        try:
+            return model.named_steps['regression'].coef_.shape[0]
+        except KeyError:
+            try:
+                return model.named_steps['model'].coef_.shape[0]
+            except KeyError:
+                return len(model.named_steps['transform'])
+    elif isinstance(model,LinearRegression):
+        return model.coef_.shape[0] 
+    else:
+        raise ValueError('Model type not recognized')
+
+
+class OneDPiecewiseFunction:
+
+    def __init__(self,models: List[Any],
+                              intervals: List[Tuple[int]]) -> None:
+        self.models = models
+        self.intervals = intervals
+
+    def predict(self,x: np.ndarray) -> np.ndarray:
+        y = np.zeros_like(x)
+        for i,model in enumerate(self.models):
+            mask = np.all([x >= self.intervals[i][0],x <= self.intervals[i][1]],axis = 0)
+            y[mask,...] = model.predict(x[mask,...])
+        return y
+    
+    def __call__(self,x: np.ndarray) -> np.ndarray:
+        return self.predict(x)
+    
