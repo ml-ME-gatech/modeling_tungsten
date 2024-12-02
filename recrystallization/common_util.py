d44e34b5c574b4fed7dfdbf96fc9cdaa242530d6
ml-ME-gatech <61564229+ml-ME-gatech@users.noreply.github.com>
2024-12-02 07:09:32 -0500

    Restructuring of model classses
    Changed the structure of how Arrhenius processes are shared among jupyter notebooks (self contained classes now) with interfaces similar to sklearn & scipy for fit/predict.
    
    Added support for the "fudge factor" Arrhenius model.

--- recrystallization/common_util.py
+++ recrystallization/common_util.py
@@ -2,6 +2,10 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import AutoMinorLocator
 import numpy as np
 from scipy.stats import t as tdist
+from abc import abstractmethod,ABC
+from statsmodels.regression.linear_model import OLS, OLSResults
+from typing import Tuple
+from scipy.optimize import minimize_scalar
 
 def setup_axis_default(ax: plt.Axes):
     
@@ -60,3 +64,132 @@ class SimpleLinearRegression:
         yhat = self.predict(xnew)
         ci_yhat = v*self.variance*(1 + 1/self.n + (xnew - self.xbar)**2/self.S_xx)**0.5
         return np.array([yhat - ci_yhat,yhat + ci_yhat])  
+    
+
+class ArrheniusProcess(ABC):
+
+    _p = None
+    def __init__(self,params: np.ndarray = None):
+        self.params = params
+
+    @abstractmethod
+    def tform(self,x: np.ndarray):
+        pass
+
+    @property
+    def p(self):
+        return self._p
+
+    @abstractmethod
+    def fit(self,X: np.ndarray,y: np.ndarray):
+        pass
+
+    @abstractmethod
+    def log_predict(self,x: np.ndarray):
+        pass
+    
+    @abstractmethod
+    def parameter_confidence_interval(self):
+        pass
+
+    def predict(self,x: np.ndarray):
+        return np.exp(self.log_predict(x))
+    
+    def __call__(self, x: np.ndarray):
+        return self.predict(x)
+    
+class LogLinearArrhenius(ArrheniusProcess):
+    
+    _p = 2
+
+    def tform(self, x: np.ndarray):
+        return np.hstack([np.ones_like(x[:,np.newaxis]),
+                          1/x[:,np.newaxis]])
+    
+    def log_predict(self,x: np.ndarray):
+        return self.tform(x).dot(self.params)
+    
+    def fit(self,x: np.ndarray,y: np.ndarray):
+        self.ols_results = OLS(np.log(y),self.tform(x)).fit()
+        self.params = self.ols_results.params.squeeze()
+        return self
+    
+    def parameter_confidence_interval(self,alpha: float):
+        return self.ols_results.conf_int(alpha)
+    
+class FudgeFactorArrhenius(LogLinearArrhenius):
+
+    _p = 3
+
+    def parameter_confidence_interval(self,alpha: float):
+        pass
+
+    def tform(self, x: np.ndarray):
+        return np.hstack([np.ones_like(x[:,np.newaxis]),
+                          1/x[:,np.newaxis]**self.params[2]])
+    
+    def log_predict(self,x: np.ndarray):
+        return self.tform(x).dot(self.params[:-1])
+    
+    def parameter_confidence_interval(self,alpha: float):
+        params_ = self.params.copy()
+
+        def _func(beta: float):
+            self.params[2] = beta
+            log_yhat = self.log_predict(self.x)
+            return np.linalg.norm(self.logy - log_yhat)
+        
+        ci = self.ols_results.conf_int(alpha)
+
+        beta_ci = []
+        for i in range(2):
+            self.params[:-1] = ci[i,:].squeeze()
+            opt_result = minimize_scalar(_func,bounds = self.beta_bounds,bracket = self.beta_bounds)
+            if opt_result.success:
+                beta_ci.append(opt_result.x)
+            else:
+                raise RuntimeError('failed to find confidence interval for beta')
+        
+        ci = np.concatenate([ci,np.array(beta_ci)[np.newaxis,:]],axis = 0)
+        self.params = params_
+        return ci
+
+    def fit(self,x: np.ndarray,
+                 y: np.ndarray,
+                 beta_init: float,
+                 beta_bounds: Tuple[float] = (0.1,2.0)):
+        
+        self.x = x.copy()
+
+        self.params = np.zeros(3)
+        self.params[2] = beta_init
+        self.logy = np.log(y)
+        self.beta_bounds = beta_bounds
+        
+        def _func(beta: float):
+            self.params[2] = beta
+            X_ = self.tform(x)
+            ols_results = OLS(self.logy,X_).fit()
+            log_yhat = ols_results.predict(X_)
+            return np.linalg.norm(self.logy - log_yhat)
+        
+        opt_result = minimize_scalar(_func,bracket = beta_bounds,bounds = beta_bounds)
+        if opt_result.success:
+            self.params[2] = opt_result.x
+        else:
+            raise RuntimeError('failed to fit fudge factor arrhenius model')
+        
+        self.ols_results = OLS(self.logy,self.tform(x)).fit()
+        self.params[:-1]= self.ols_results.params.squeeze()
+
+        return self
+    
+        
+
+
+            
+
+
+
+
+        
