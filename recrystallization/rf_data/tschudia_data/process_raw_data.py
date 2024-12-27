7a7b313dd8f53aa8bd99d5a2535c8a83765e82d1
ml-ME-gatech <mlanahan3@gatech.edu>
2024-12-26 20:26:40 -0500

    Non-isothermal GLM work
    Some issues pinning down the formulation.

--- recrystallization/rf_data/tschudia_data/process_raw_data.py
+++ recrystallization/rf_data/tschudia_data/process_raw_data.py
@@ -3,17 +3,18 @@ import numpy as np
 
 def main():
 
-    df = pd.read_csv('tschudia_data_raw.csv',header = 1)
-    with open('tschudia_data_raw.csv', 'r') as f:
-        headers = [s.strip() for s in f.readline().split(',') if s.strip() and 'error' not in s]
+    df = pd.read_csv('tschudia_temp_1100C.csv',header = 1)
+    with open('tschudia_temp_1100C.csv', 'r') as f:
+        headers = [s.strip() for s in f.readline().split(',') if s.strip() and 'error' not in s.lower()]
 
+    print(headers)
     for i in range(len(headers)):
         data = df.iloc[:,i*4:i*4+4]
-        data.columns = ['temperature','hardness','none','hardness error']
-        data = data[['temperature','hardness','hardness error']]
+        data.columns = ['time [hr]','hardness','none','hardness error']
+        data = data[['time [hr]','hardness','hardness error']]
         data.loc[:,'hardness error'] = np.abs(data['hardness error'].to_numpy() - data['hardness'].to_numpy())
         data.dropna(inplace= True)
-        data.to_csv('tschudia_data_{}.csv'.format(headers[i]),index=False)
+        data.to_csv('tschudia_data_T_1100C{}.csv'.format(headers[i]),index=False)
 
 if __name__ == '__main__':
     main()
