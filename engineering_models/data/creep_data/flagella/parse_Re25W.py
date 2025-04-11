65f370a698db83d3f03252fba19ea9197119a8ea
Mike <mlanahan3@gatech.edu>
2025-04-11 14:12:14 -0400

    Finished Creep Modeling
    Developed Model for creep and finished subsequent analysis.

--- engineering_models/data/creep_data/flagella/parse_Re25W.py
+++ engineering_models/data/creep_data/flagella/parse_Re25W.py
@@ -1,14 +1,51 @@
 import numpy as np
 import pandas as pd
+from functools import wraps
+from typing import Callable
+
+def maybe_nan(func: Callable) -> Callable:
+
+    @wraps(func)
+    def wrapped(s: str):
+        if s == '-':
+            return np.nan
+        return  func(s)
+    
+    return wrapped
 
 def main():
 
-    with open('raw/Re25W_raw.csv','rb') as f:
+    transforms = [lambda x: x,
+                  lambda x: int(x.replace(',','')),
+                  lambda x: int(x.replace(',','')),
+                  lambda x: np.nan,
+                  lambda x: np.nan, 
+                  lambda x: float(x),
+                  lambda x: int(x.replace(',',''))]
+    
+    transforms = [maybe_nan(t) for t in transforms] 
+
+    with open('Re25W_raw.csv','r') as f:
         columns = f.readline().strip().split(',')
         data  = []
         for line in f.readlines():
-            dat = line.strip().split("")[1]
-            print(dat)
+            line = ''.join(line.strip().split(',')[:-6])
+            if line[0] == '"':
+                line = line[1:]
+            if line[-1] == '"':
+                line = line[:-1]
+            
+            dat = [t(s) for t,s in zip(transforms,line.split(" "))]
+            data.append(dat)
+
+    
+    data = pd.DataFrame(data, columns=columns)
+    data = data[['Material Source','Test Temperature (°C)', 'Stress (psi)',
+                 'Time to Rupture (hr)',
+                  'Total Elongation (%)']]
+    data.dropna(inplace= True)
+    data.to_csv('../Re25WSheet.csv')   
+            
 
 
     
